702e8c3712976b360d7f798881d91c17dd2547fb
Bokyeong Ju <87298145+jupyter1234@users.noreply.github.com>
2024-02-08 10:34:55 +0900

    update

--- day16/72413-합승택시요금/jupyter1234.py
+++ day16/72413-합승택시요금/jupyter1234.py
@@ -1,34 +1,25 @@
 import heapq
 def solution(n, s, a, b, fares):
     #방향없음
-    
+    #그래프 생성
     graph = [[] for _ in range(n+1)]
     for u,v,c in fares:
         graph[u].append((v,c))
         graph[v].append((u,c))
     
-    #각각 따로 갈 때
+    #각 노선까지 거리
     dist = dj(s,graph,n)
-    a_d = dist[a]
-    b_d = dist[b]
-    #같이 갈 때
+    
+    #최소거리 구하기
     min_dist = [float('inf')] * (n+1)
     for v in range(1,n+1):
         min_dist[v] = get_dist(v,a,b,graph,n) + dist[v]
-    index = min_dist.index(min(min_dist))
-    p = min(min_dist)
-    print(index,p)
-    if a_d < b_d:
-        p,d = a,b
-    else:
-        p,d = b,a
-    a_to_b = dj(p,graph,n)[d]
-    return min_dist[index]
-    # print(a_d,b_d,a_to_b)
-    # print(min(a_d+b_d,a_d + a_to_b))
-    #다익스트라
-    # return answer
+    # index = min_dist.index(min(min_dist))
+    # p = min(min_dist)
+    # print(index,p)
+    return min(min_dist)
 
+#다익스트라
 def dj(v,graph,n):
     heap = []
     dist = [float('inf')] * (n+1)
@@ -41,7 +32,6 @@ def dj(v,graph,n):
         for x in graph[v]:
             nxt, nc = x
             if c + nc < dist[nxt]:
-                visited[nxt] = True
                 dist[nxt] = nc + c
                 heapq.heappush(heap,(nxt,nc+c))
     return dist
@@ -54,4 +44,3 @@ def get_dist(v,a,b,graph,n):
 #s : 출발지점
 # 최저 예상 택시요금 (합승 안해도 됨)
 #fares [지점1,지점2,가격]
-
