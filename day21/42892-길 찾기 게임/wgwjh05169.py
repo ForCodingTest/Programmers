44cda80f0bd6d2c0d218036be8189be59db9c98d
HyeonJeong <117988360+wgwjh05169@users.noreply.github.com>
2024-02-13 22:24:48 +0900

    solve: 길찾기 게임

---
+++ day21/42892-길 찾기 게임/wgwjh05169.py
@@ -0,0 +1,60 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+pre = []
+post = []
+
+def solution(nodeinfo):
+    nodeinfo = [[i + 1, *nodeinfo[i], None, None] for i in range(len(nodeinfo))]
+    nodeinfo.sort(key = lambda x: [-x[2], x[1]])
+    
+    tree = get_tree(nodeinfo)
+    
+    preorder(tree)
+    postorder(tree)
+    
+    return pre, post
+
+
+def get_tree(nodeinfo):
+    tree = None
+    for node in nodeinfo:
+        if not tree:
+            tree = node
+            continue
+            
+        parent = tree
+        while parent:
+            if node[1] < parent[1]:
+                if not parent[3]:
+                    parent[3] = node
+                    parent = parent[3]
+                parent = parent[3]
+            else:
+                if not parent[4]:
+                    parent[4] = node
+                    parent = parent[4]
+                parent = parent[4]
+                    
+    return tree
+    
+    
+def preorder(node):
+    pre.append(node[0])
+    if node[3]:
+        preorder(node[3])
+    if node[4]:
+        preorder(node[4])
+
+    
+def postorder(node):
+    if node[3]:
+        postorder(node[3])
+    if node[4]:
+        postorder(node[4])
+    post.append(node[0])
+
+
+# tree 만들기 오랜만이라 삽질 좀 했네요
+# 전위 중위 후위 순회도 오랜만
+# 재귀 제한 안 늘려줘서 채점 시 두 개 런타임 났었다... 재귀 풀이엔 set recursion limit 기억하기!
